ae404abd4f1c6dc05ce30f2a23993f8608d070f8
Lucas <lucaspereiradelima2020@gmail.com>
2025-12-29 20:19:53 -0300

    Adicionando produtos na lista

---
+++ src/desafios/desafio02/Principal.java
@@ -0,0 +1,18 @@
+package desafios.desafio02;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("Celular", 1199.99, 12);
+        Produto produto2 = new Produto("Televisão", 670.00, 23);
+        Produto produto3 = new Produto("PlayStation 4", 1799.99, 30);
+        Produto produto4 = new Produto("Computador", 2999.99, 15);
+
+        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
+        listaDeProdutos.add(produto1);
+        listaDeProdutos.add(produto2);
+        listaDeProdutos.add(produto3);
+        listaDeProdutos.add(produto4);
+    }
+}
