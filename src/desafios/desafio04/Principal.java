643462a95a2ea52053c7981fe632209356d148eb
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-01 18:02:25 -0300

    Usando a interface List e fazendo conversão para outros tipos de Lista

--- src/desafios/desafio04/Principal.java
+++ src/desafios/desafio04/Principal.java
@@ -1,7 +1,22 @@
+/**
+ * 1. Crie uma lista de números inteiros e utilize o método Collections.sort para ordená-la em ordem crescente.
+ *    Em seguida, imprima a lista ordenada.
+ * 2. Crie uma classe Titulo com um atributo nome do tipo String. Implemente a interface Comparable na classe
+ *    para que seja possível ordenar uma lista de objetos Titulo.
+ * 3. No Exercício 2, crie alguns objetos da classe Titulo e adicione-os a uma lista. Utilize o
+ *    método Collections.sort para ordenar a lista e, em seguida, imprima os títulos ordenados.
+ * 4. Crie uma lista utilizando a interface List e instancie-a tanto como ArrayList quanto como LinkedList.
+ *    Adicione elementos e imprima a lista, mostrando que é possível trocar facilmente a implementação.
+ * 5. Modifique o Exercício 4 para declarar a variável de lista como a interface List, demonstrando
+ *    o uso de polimorfismo.
+ */
+
 package desafios.desafio04;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Principal {
     public static void main(String[] args) {
@@ -34,5 +49,30 @@ public class Principal {
         System.out.println("Lista de Títulos = " + titulos);
         Collections.sort(titulos);
         System.out.println("Lista de Títulos em ordem alfabética = " + titulos);
+
+        List<String> afazeresComArrayList = new ArrayList<>();
+        afazeresComArrayList.add("Arrumar a cama");
+        afazeresComArrayList.add("Escovar os dentes");
+        afazeresComArrayList.add("Tomar café da manhã");
+        System.out.println("Lista com Array List = " + afazeresComArrayList);
+
+        List<String> afazeresComLinkedList = new LinkedList<>();
+        afazeresComLinkedList.add("Almoço");
+        afazeresComLinkedList.add("Trabalhar");
+        afazeresComLinkedList.add("Tomar banho");
+        System.out.println("Lista com Linked List = " + afazeresComLinkedList);
+
+        List<String> tecnologias;
+        tecnologias = new ArrayList<>();
+        tecnologias.add("HTML");
+        tecnologias.add("CSS");
+        tecnologias.add("JavaScript");
+        System.out.println("Tecnologias com ArrayList = " + tecnologias);
+
+        tecnologias = new LinkedList<>();
+        tecnologias.add("React");
+        tecnologias.add("Angular");
+        tecnologias.add("PostgreSQL");
+        System.out.println("Tecnologias com LinkedList = " + tecnologias);
     }
 }
